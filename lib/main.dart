64dcb818dcc80375fcc65034eb20496b31100526
Jedicillo <seron.cesar@gmail.com>
2022-08-25 14:53:14 -0400

    2.6 listo. Se navega a nueva ruta con los nombres guardados

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,82 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Generador de Nombres"),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: "Sugerencias guardadas",
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel:
+                  alreadySaved ? "Quitar de los guardados" : "Guardar",
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = tiles.isNotEmpty ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+          ).toList() : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Sugerencias Guardadas"),
+            ),
+            body: ListView(children: divided,),
+          );
         }
-
-        final alreadySaved = _saved.contains(_suggestions[index]);
-
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? "Quitar de los guardados" : "Guardar",
-          ),
-          onTap: () {
-            setState(() {
-              if(alreadySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });
-          },
-        );
-      },
+      )
     );
   }
 }
@@ -63,17 +106,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Generador de Nombres',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bienvenido al generador de nombres de Flutter'),
-        ),
-        body: const Center(
-          //child: Text('Hola Mundo'),
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords()
     );
   }
 }
